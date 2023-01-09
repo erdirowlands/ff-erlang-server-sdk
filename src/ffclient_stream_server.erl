1ae523139c458e923d35c47cab2773d3ddd8744f
Erdi Rowlands <erdi.rowlands@harness.io>
2023-01-09 11:59:52 +0000

    FFM-6137 Add streaming POC code

--- src/ffclient_stream_server.erl
+++ src/ffclient_stream_server.erl
@@ -24,3 +24,26 @@ handle_call(Request, From, State) ->
 
 handle_cast(Request, State) ->
   erlang:error(not_implemented).
+
+stream() ->
+  CommonOpts = #{content_handlers => [gun_sse_h, gun_data_h]},
+  {ok, Pid} = gun:open("config.ff.harness.io", 443, #{
+%%    http_opts => CommonOpts
+    http_opts => CommonOpts,
+    transport => tls,
+    protocols => [http]
+  }),
+
+  Ref = gun:get(Pid, "/api/1.0/stream?cluster=2", #{
+    <<"accept">> => <<"text/event-stream">>,
+    <<"API-Key">> => <<"">>,
+    <<"Authorization">> => <<"Bearer ">> }
+  ),
+
+  {response, nofin, Code, RespHeaders} = gun:await(Pid, Ref).
+%%  {sse, Event} = gun:await(Pid, Ref),
+%%  #{
+%%    last_event_id := LastEventID,
+%%    event_type := <<"message">>,
+%%    data := Data
+%%  } = Event.
