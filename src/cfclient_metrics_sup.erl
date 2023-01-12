8e2d10cef78c36fa2cdf05309722f7950352a6bd
Erdi Rowlands <erdi.rowlands@harness.io>
2023-01-12 11:49:27 +0000

    FFM-6398 Add metrics supervisor
    FFM-6398 Add metrics supervisor

---
+++ src/cfclient_metrics_sup.erl
@@ -0,0 +1,55 @@
+%%%-------------------------------------------------------------------
+%%% @doc Metrics supervisor
+%%% @end
+%%%-------------------------------------------------------------------
+-module(cfclient_metrics_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+%% @doc Starts the supervisor
+-spec(start_link() -> {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
+start_link() ->
+  supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+
+%% @private
+%% @doc Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart frequency and child
+%% specifications.
+-spec(init(Args :: term()) ->
+  {ok, {SupFlags :: {RestartStrategy :: supervisor:strategy(),
+    MaxR :: non_neg_integer(), MaxT :: non_neg_integer()},
+    [ChildSpec :: supervisor:child_spec()]}}
+  | ignore | {error, Reason :: term()}).
+init([]) ->
+  MaxRestarts = 10,
+  MaxSecondsBetweenRestarts = 3600,
+  SupFlags = #{strategy => simple_one_for_one,
+    intensity => MaxRestarts,
+    period => MaxSecondsBetweenRestarts},
+
+  ChildSpec = #{
+    %% `id` key is ignored if provided in a simple_one_for_one strategy so don't provide it
+    start => {cfclient_metrics_server, start_link, []}, %% The args list is empty here, but when start_child/2 is called this list will be appended with the required args
+    restart => permanent,
+    shutdown => 5000,
+    type => worker,
+    modules => [cfclient_metrics_server]},
+
+  {ok, {SupFlags, [ChildSpec]}}.
