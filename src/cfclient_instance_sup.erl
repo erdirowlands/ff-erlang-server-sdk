b6f0b1fcd5e229d1f6579c8625f0affebb54eaa4
Erdi Rowlands <erdi.rowlands@harness.io>
2023-01-16 22:27:59 +0000

    FFM-6398 Some caches are now starting

--- src/cfclient_instance_sup.erl
+++ src/cfclient_instance_sup.erl
@@ -90,11 +90,11 @@ init([InstanceName, FeatureCacheName, PollSupChildName, MetricsSupChildName, IsA
 %%%===================================================================
 
 instance_children(InstanceName, FeatureCacheName, PollSupName, MetricsSupName, analytics_enabled) ->
-  CacheWorkerChild = ?INSTANCE_CHILD(FeatureCacheName, ?FEATURE_CACHE_MODULE, [{local, FeatureCacheName}, ?FEATURE_CACHE_SIZE, []], worker),
+  CacheWorkerChild = ?INSTANCE_CHILD(FeatureCacheName, ?FEATURE_CACHE_MODULE, [[{local, FeatureCacheName}, ?FEATURE_CACHE_SIZE, []]], worker),
   PollSupChild = ?INSTANCE_CHILD(PollSupName, ?POLL_SUPERVISOR, [InstanceName], supervisor),
   MetricsSupChild = ?INSTANCE_CHILD(MetricsSupName, ?METRICS_SUP_MODULE, [MetricsSupName], supervisor),
   [CacheWorkerChild, PollSupChild, MetricsSupChild];
 instance_children(InstanceName, FeatureCacheName, PollSupName, _, analytics_disabled) ->
-  CacheWorkerChild = ?INSTANCE_CHILD(FeatureCacheName, ?FEATURE_CACHE_MODULE, [{local, FeatureCacheName}, ?FEATURE_CACHE_SIZE, []], worker),
+  CacheWorkerChild = ?INSTANCE_CHILD(FeatureCacheName, ?FEATURE_CACHE_MODULE, [[{local, FeatureCacheName}, ?FEATURE_CACHE_SIZE, []]], worker),
   PollSupChild = ?INSTANCE_CHILD(PollSupName, ?POLL_SUPERVISOR, [InstanceName], supervisor),
   [CacheWorkerChild, PollSupChild].
--- src/cfclient_metrics_sup.erl
+++ src/cfclient_metrics_sup.erl
@@ -82,7 +82,7 @@ init([InstanceName]) ->
 metrics_children(InstanceName) ->
   {EvaluationCacheName, TargetCacheName, ServerName} = get_refs_from_instance(InstanceName),
 
-  MetricsEvaluationCacheChild = ?METRICS_CHILD(EvaluationCacheName, ?LRU_MODULE, [{local, EvaluationCacheName}, ?CACHE_SIZE, []], worker),
+  MetricsEvaluationCacheChild = ?METRICS_CHILD(EvaluationCacheName, ?LRU_MODULE, [[{local, EvaluationCacheName}], ?CACHE_SIZE, []], worker),
   MetricsTargetCacheChild = ?METRICS_CHILD(TargetCacheName, ?LRU_MODULE, [{local, TargetCacheName}, ?CACHE_SIZE, []], worker),
   MetricsServerChild = ?METRICS_CHILD(ServerName, ?METRICS_SERVER_MODULE, [ServerName, InstanceName], worker),
   [MetricsEvaluationCacheChild, MetricsTargetCacheChild, MetricsServerChild].
